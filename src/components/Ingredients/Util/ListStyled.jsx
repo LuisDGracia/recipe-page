34f0e9dd7387889affc28267c6abbe1aec6d3258
Zuzki <ldegrac071@hotmail.com>
2021-02-03 15:19:36 -0500

    styled checkbox

--- src/components/Ingredients/Util/ListStyled.jsx
+++ src/components/Ingredients/Util/ListStyled.jsx
@@ -11,18 +11,40 @@ const IngredientItem = styled.li`
 const Label = styled.label`
 	width: 90%;
 	display: inline-block;
+
+	&:hover{
+		cursor: pointer;
+	}
 `;
 
 const CheckBox = styled.input`
+	width: 25px;
+	height: 25px;
+
+	border: 2px solid;
+	border-color: gray;
+	border-radius: 3px;
+
 	position: relative;
 
-	width: 20px;
-	height: 20px;
+	appearance: none;
+	outline: 0;
+	transition: background 175ms cubic-bezier(0.1, 0.1, 0.25, 1);
+
+	&:checked {
+		color: white;
+		border-color: gray;
+		background: rgba( 0, 255, 0, 0.5 );
+	}
 
 	&:checked + ${Label} {
-		color: rgba( 0, 0, 0, 0.2 );
+		color: rgba(0, 0, 0, 0.2);
 		text-decoration: line-through;
 		text-decoration-color: black;
+		
+
+	&:hover {
+		cursor: pointer;
 	}
 `;
 
