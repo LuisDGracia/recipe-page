c7b740ec09adce987ecb11348a5f76f138f3ba1f
Zuzki <ldegrac071@hotmail.com>
2021-02-03 10:32:27 -0500

    instructions finished

--- src/components/Instructions/InstructionsStyled.jsx
+++ src/components/Instructions/InstructionsStyled.jsx
@@ -1,7 +1,47 @@
 import styled from 'styled-components'
 
 const InstructionsContainer = styled.div`
-  order: 3;
+	order: 3;
+
+	@media screen and ( min-width: 426px ){
+		width: 60%;
+	}
 `
 
-export { InstructionsContainer }
+const InstructionsList = styled.ol`
+	padding: 0;
+	list-style: none;
+	counter-reset: item;
+
+	li{
+		counter-increment: item;
+		margin-bottom: 15px;
+		
+		display: flex;
+	}
+
+	li::before {
+		height: 20px;
+		width: 20px;
+
+		margin-right: 10px;
+		border-radius: 5px;
+		padding: 5px;
+
+		background-color: orange;
+		text-align: center;
+		color: white;
+
+		content: counter(item);
+
+		display: inline-block;
+	}
+`;
+
+const Text = styled.p`
+	width: calc( 100% - 40px );
+
+	display: inline-block;
+`
+
+export { InstructionsContainer, InstructionsList, Text }
